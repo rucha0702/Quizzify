f53cb19e4f40aaa6c40aee187ed79ccdb98ce14a
rucha0702 <rucha.kasture2002@gmail.com>
2023-04-12 23:51:29 +0530

    cors

--- src/components/user/dashboard/AllQuizzesData.js
+++ src/components/user/dashboard/AllQuizzesData.js
@@ -38,7 +38,7 @@ const AllQuizzesData = () => {
 
 useEffect(()=>{
   const getCurrentTime = async () =>{
-    const data = await fetch('https://worldtimeapi.org/api/timezone/Asia/Kolkata')
+    const data = await fetch('https://worldtimeapi.org/api/timezone/Asia/Kolkata',{ mode: 'no-cors' })
       .then(response => response.json())
       .then(data => {
         const currentTime = data.datetime;
